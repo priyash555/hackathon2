fa23a858361ac2208e26bb89cff9ecd3fb895f80
priyash555 <31501889+priyash555@users.noreply.github.com>
2019-04-11 02:50:28 +0530

    first

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth/auth-guard.service';
+import { HomeComponent } from '../home/home.component';
+import { NewsComponent } from '../news/news.component';
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, {
+  path: 'auth',
+  loadChildren: 'app/auth/auth.module#AuthModule'
+}, {
+  path: 'admin',
+  loadChildren: 'app/admin/admin.module#AdminModule'
+}, {
+  path: 'news',
+  component: NewsComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [AuthGuard],
+  declarations: []
+})
+
+export class AppRoutingModule {}
